43bddb9c37cd85bc631d3b2056c8d2a4ec098577
jtlaughton <jtlaughton@gmail.com>
2025-05-31 21:27:01 -0500

    make changes to addr location assumption

--- modmap.c
+++ modmap.c
@@ -114,6 +114,8 @@ modmap_ioctl(struct cdev *dev, u_long cmd, caddr_t addr, int flags,
                 break;
             }
 
+            void * __kerncap mapped_addr = (void * __kerncap)td->td_retval[0];
+
             kern_req_user->addr = kern_req.addr;
             kern_req_user->len = kern_req.len;
             kern_req_user->prot = kern_req.prot;
@@ -123,15 +125,14 @@ modmap_ioctl(struct cdev *dev, u_long cmd, caddr_t addr, int flags,
             kern_req_user->extra = NULL;
 
             uprintf("First Copyoutcap\n");
-            void * __capability user_mapped_addr;
-            error = copyoutcap(&kern_req.addr, &user_mapped_addr, sizeof(void *));
+            error = copyoutcap(&mapped_addr, &(kern_req_user->addr), sizeof(void *));
             if(error != 0){
                 uprintf("Here's the addr: %p\n", kern_req.addr);
                 uprintf("Copuoutcap error: %d\n", error);
                 break;
             }
 
-            kern_req_user->addr = user_mapped_addr;
+            uprintf("Here's success addr: %p\n", kern_req_user->addr);
 
             uprintf("First Copyout\n");
             error = copyout(&kern_cap_req, kern_req_user->extra, sizeof(kern_cap_req));
